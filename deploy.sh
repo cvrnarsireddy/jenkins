a46e6bff10108aae0f2cd25571a27336e90e23ad
asha0404 <asharani.rajanna@gmail.com>
2022-05-20 16:30:13 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 this code is for mster branch
 this is for testing satsh command
+=======
+>>>>>>> 8dc1263 (dev branch script updated)
 this is updating in development branch
 this file is for deployment purpose
 this is again updated
